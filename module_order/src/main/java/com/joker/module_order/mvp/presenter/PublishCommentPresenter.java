af07b5274bb9156717df9633b244d77bfbf89223
JIE-Joker <2575490085@qq.com>
2019-04-01 22:19:24 +0800

    Commit

--- module_order/src/main/java/com/joker/module_order/mvp/presenter/PublishCommentPresenter.java
+++ module_order/src/main/java/com/joker/module_order/mvp/presenter/PublishCommentPresenter.java
@@ -1,15 +1,26 @@
 package com.joker.module_order.mvp.presenter;
 
 import android.app.Application;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.example.commonres.beans.Comment;
+import com.example.commonres.beans.Hotel;
+import com.example.commonres.beans.Order;
 import com.jess.arms.di.scope.ActivityScope;
 import com.jess.arms.http.imageloader.ImageLoader;
 import com.jess.arms.integration.AppManager;
 import com.jess.arms.mvp.BasePresenter;
 import com.joker.module_order.mvp.contract.PublishCommentContract;
 
+import java.util.HashMap;
+
 import javax.inject.Inject;
 
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.SaveListener;
+import cn.bmob.v3.listener.UpdateListener;
 import me.jessyan.rxerrorhandler.core.RxErrorHandler;
 
 
@@ -36,5 +47,52 @@ public class PublishCommentPresenter extends BasePresenter<PublishCommentContrac
         this.mAppManager = null;
         this.mImageLoader = null;
         this.mApplication = null;
+    }
+
+
+    /**
+     * 发布评论
+     * @param mOrder
+     * @param commentContent
+     * @param commentHasMap
+     */
+    public void publishComment(Order mOrder, String commentContent,
+                               HashMap<String,Integer> commentHasMap){
+        Comment comment = new Comment();
+        comment.setOrder(mOrder);
+        comment.setHotel(mOrder.getHotel());
+        comment.setUser(mOrder.getUser());
+        comment.setContent(commentContent);
+        final Double grade = (commentHasMap.get("environment") + commentHasMap.get("condition") + commentHasMap.get("service")) / 3.0;
+        comment.setGrade(grade);
+        comment.save(new SaveListener<String>() {
+            @Override
+            public void done(String s, BmobException e) {
+                if (e == null) {
+                    mOrder.setState(5);
+                    mOrder.update(new UpdateListener() {
+                        @Override
+                        public void done(BmobException e) {
+                            if (e == null) {
+                                Hotel hotel = new Hotel();
+                                hotel.setObjectId(mOrder.getHotel().getObjectId());
+                                hotel.increment("comment");
+                                hotel.update(new UpdateListener() {
+                                    @Override
+                                    public void done(BmobException e) {
+                                        if (e == null)
+                                            mRootView.publishCommentResult(true,"评论成功");
+                                        else
+                                            mRootView.publishCommentResult(false,"评论增加失败" + e.toString());
+                                    }
+                                });
+                            } else
+                                mRootView.publishCommentResult(false,"发表评论失败" + e.toString());
+                        }
+                    });
+                } else
+                    mRootView.publishCommentResult(false,"发表评论失败" + e.toString());
+            }
+        });
     }
 }
--- module_order/src/main/java/com/joker/module_order/mvp/view/activity/PublishCommentActivity.java
+++ module_order/src/main/java/com/joker/module_order/mvp/view/activity/PublishCommentActivity.java
@@ -4,10 +4,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import com.alibaba.android.arouter.facade.annotation.Route;
+import com.example.commonres.beans.Order;
+import com.example.commonres.utils.ToastUtil;
+import com.example.commonsdk.core.RouterHub;
 import com.jess.arms.base.BaseActivity;
 import com.jess.arms.di.component.AppComponent;
 import com.jess.arms.utils.ArmsUtils;
@@ -18,16 +26,23 @@ import com.joker.module_order.di.module.PublishCommentModule;
 import com.joker.module_order.mvp.contract.PublishCommentContract;
 import com.joker.module_order.mvp.presenter.PublishCommentPresenter;
 
+import java.util.HashMap;
+
 import butterknife.BindView;
+import butterknife.OnClick;
 
 import static com.jess.arms.utils.Preconditions.checkNotNull;
 
-
-public class PublishCommentActivity extends BaseActivity<PublishCommentPresenter> implements PublishCommentContract.View {
+/**
+ * 订单Fragment -> 待评价订单Fragment -> 评价
+ */
+@Route(path = RouterHub.ORDER_PUBLISHCOMMENTACTIVITY)
+public class PublishCommentActivity extends BaseActivity<PublishCommentPresenter>
+        implements PublishCommentContract.View {
 
     //评论内容
     @BindView(R2.id.et_comment)
-    EditText commentContent;
+    EditText commentContentView;
     //环境评分
     @BindView(R2.id.environment_comment_ll)
     LinearLayout environmentScore;
@@ -40,6 +55,9 @@ public class PublishCommentActivity extends BaseActivity<PublishCommentPresenter
     //提交按钮
     @BindView(R2.id.tv_publish_comment)
     Button publishComment;
+
+    private Order mOrder;
+    private HashMap<String, Integer> commentHasMap = new HashMap<>();
 
     @Override
     public void setupActivityComponent(@NonNull AppComponent appComponent) {
@@ -58,7 +76,82 @@ public class PublishCommentActivity extends BaseActivity<PublishCommentPresenter
 
     @Override
     public void initData(@Nullable Bundle savedInstanceState) {
+        mOrder = (Order) getIntent().getSerializableExtra("Order");
+    }
 
+    @OnClick({R2.id.tv_publish_comment,R2.id.back})
+    public void click(View view){
+        int viewId = view.getId();
+        if (view.getTag() != null){
+            String tag = (String) view.getTag();
+            int index = Integer.parseInt(tag);
+            LinearLayout layout = (LinearLayout) view.getParent();
+            int layoutId = layout.getId();
+            if (layoutId == R.id.environment_comment_ll){
+                for (int i = 0; i < environmentScore.getChildCount(); i++) {
+                    ImageView imageView = (ImageView) environmentScore.getChildAt(i);
+                    if (i < index) {
+                        commentHasMap.put("environment", index);
+                        imageView.setImageResource(R.mipmap.comment_like);
+                    } else
+                        imageView.setImageResource(R.mipmap.comment_no_like);
+                }
+            }else if (layoutId == R.id.hotel_condition_comment_ll){
+                for (int i = 0; i < hotelConditionScore.getChildCount(); i++) {
+                    ImageView imageView = (ImageView) hotelConditionScore.getChildAt(i);
+                    if (i < index) {
+                        commentHasMap.put("condition", index);
+                        imageView.setImageResource(R.mipmap.comment_like);
+                    } else
+                        imageView.setImageResource(R.mipmap.comment_no_like);
+                }
+            }else if (layoutId == R.id.service_comment_ll) {
+                for (int i = 0; i < serviceScore.getChildCount(); i++) {
+                    ImageView imageView = (ImageView) serviceScore.getChildAt(i);
+                    if (i < index) {
+                        commentHasMap.put("service", index);
+                        imageView.setImageResource(R.mipmap.comment_like);
+                    } else
+                        imageView.setImageResource(R.mipmap.comment_no_like);
+                }
+            }
+        }
+
+        if (viewId == R.id.back){//返回，取消评论
+            Intent intent = new Intent();
+            intent.putExtra("Comment", false);
+            setResult(1, intent);
+            finish();
+        }else if (viewId == R.id.tv_publish_comment){
+            String commentContent = commentContentView.getText().toString().trim();
+            if (commentContent.equals(""))
+                ToastUtil.makeText(this,"评论内容不能为空");
+            else {
+                if (commentHasMap.get("condition") == null || commentHasMap.get("service") == null ||
+                        commentHasMap.get("environment") == null)
+                    ToastUtil.makeText(this,"请打评分");
+                else
+                    mPresenter.publishComment(mOrder,commentContent,commentHasMap);
+            }
+        }
+    }
+
+
+    /**
+     * 发表评论结果
+     * @param result
+     * @param tips
+     */
+    @Override
+    public void publishCommentResult(Boolean result, String tips) {
+        if (result){
+            ToastUtil.makeText(this, tips);
+            Intent intent = new Intent();
+            intent.putExtra("Comment", true);
+            setResult(1,intent);
+            finish();
+        }else
+            Log.d("PublishComment", tips);
     }
 
     @Override
