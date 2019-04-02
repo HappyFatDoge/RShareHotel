5208d8f69f745259ae67db6b5775638b9d7704c6
jieaoxingzhe <2575490085@qq.com>
2019-04-02 10:17:48 +0800

    Commit

--- module_order/src/main/java/com/joker/module_order/mvp/view/activity/PublishCommentActivity.java
+++ module_order/src/main/java/com/joker/module_order/mvp/view/activity/PublishCommentActivity.java
@@ -10,7 +10,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.example.commonres.beans.Order;
