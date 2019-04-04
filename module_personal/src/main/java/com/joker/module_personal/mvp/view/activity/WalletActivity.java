e5962fb664743a849377a0e88dad345c55a203c8
jieaoxingzhe <2575490085@qq.com>
2019-04-04 17:35:04 +0800

    Commit

--- module_personal/src/main/java/com/joker/module_personal/mvp/view/activity/WalletActivity.java
+++ module_personal/src/main/java/com/joker/module_personal/mvp/view/activity/WalletActivity.java
@@ -4,22 +4,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.View;
 
+import com.alibaba.android.arouter.facade.annotation.Route;
+import com.example.commonsdk.core.RouterHub;
 import com.jess.arms.base.BaseActivity;
 import com.jess.arms.di.component.AppComponent;
 import com.jess.arms.utils.ArmsUtils;
-
+import com.joker.module_personal.R;
+import com.joker.module_personal.R2;
 import com.joker.module_personal.di.component.DaggerWalletComponent;
 import com.joker.module_personal.di.module.WalletModule;
 import com.joker.module_personal.mvp.contract.WalletContract;
 import com.joker.module_personal.mvp.presenter.WalletPresenter;
 
-import com.joker.module_personal.R;
-
+import butterknife.OnClick;
 
 import static com.jess.arms.utils.Preconditions.checkNotNull;
 
-
+/**
+ * 个人中心Fragment -> 我的钱包
+ */
+@Route(path = RouterHub.PERSONAL_WALLETACTIVITY)
 public class WalletActivity extends BaseActivity<WalletPresenter> implements WalletContract.View {
 
     @Override
@@ -39,7 +45,14 @@ public class WalletActivity extends BaseActivity<WalletPresenter> implements Wal
 
     @Override
     public void initData(@Nullable Bundle savedInstanceState) {
+    }
 
+
+    @OnClick(R2.id.back)
+    public void onClicked(View view){
+        int viewId = view.getId();
+        if (viewId == R.id.back)
+            killMyself();
     }
 
     @Override
